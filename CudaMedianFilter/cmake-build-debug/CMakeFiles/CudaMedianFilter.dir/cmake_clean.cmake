2b3f7d1bcb88e25df20017e1c607e374ad6a024d
ArtemShakhov <64249436+ArtemShakhov@users.noreply.github.com>
2020-11-16 17:51:22 +0400

    Add files via upload

---
+++ CudaMedianFilter/cmake-build-debug/CMakeFiles/CudaMedianFilter.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CudaMedianFilter.dir/cmake_device_link.o"
+  "CMakeFiles/CudaMedianFilter.dir/medianFilter.cu.o"
+  "CudaMedianFilter"
+  "CudaMedianFilter.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CUDA)
+  include(CMakeFiles/CudaMedianFilter.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
